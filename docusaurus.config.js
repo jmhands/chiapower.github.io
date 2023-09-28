d9074539f5e7410fafe823c7e61990ea17acf723
Jonmichael Hands <jmhands@gmail.com>
2023-09-28 13:09:53 -0700

    updating docusaurus
    updating modules

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,134 +1,116 @@
-// Note: type annotations allow type checking and IDEs autocompletion
+module.exports = async function createConfigAsync() {
+    const math = (await import('remark-math')).default;
+    const katex = (await import('rehype-katex')).default;
+    const lightCodeTheme = require('prism-react-renderer/themes/github');
+    const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const math = require('remark-math');
-const katex = require('rehype-katex');
-
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'Chiapower',
-  tagline: 'Modeling the energy use for Chia blockchian',
-  url: 'https://chiapower.org/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'jmhands', // Usually your GitHub org/user name.
-  projectName: 'chiapower', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          routeBasePath: '/',
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-        googleAnalytics: {
-          trackingID: 'G-Z9SR3NERTV',
-          anonymizeIP: true,
-        },
-      }),
-    ],
-  ],
-
-  stylesheets: [
-     {
-       href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-       type: 'text/css',
-       integrity:
-         'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-       crossorigin: 'anonymous',
-     },
-   ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
+    return {
         title: 'Chiapower',
-        logo: {
-          alt: 'Chiapower Logo',
-          src: 'img/chiapower-logo.svg',
+        tagline: 'Modeling the energy use for Chia blockchain',
+        url: 'https://chiapower.org/',
+        baseUrl: '/',
+        onBrokenLinks: 'throw',
+        onBrokenMarkdownLinks: 'warn',
+        favicon: 'img/favicon.ico',
+        organizationName: 'jmhands',
+        projectName: 'chiapower',
+        i18n: {
+            defaultLocale: 'en',
+            locales: ['en'],
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Model',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/Chia-Network',
-            label: 'GitHub',
-            position: 'right',
-          },
+        presets: [
+            [
+                '@docusaurus/preset-classic',
+                {
+                    docs: {
+                        routeBasePath: '/',
+                        remarkPlugins: [math],
+                        rehypePlugins: [katex],
+                    },
+                    blog: {
+                        showReadingTime: true,
+                    },
+                    theme: {
+                        customCss: require.resolve('./src/css/custom.css'),
+                    },
+                    googleAnalytics: {
+                        trackingID: 'G-Z9SR3NERTV',
+                        anonymizeIP: true,
+                    },
+                },
+            ],
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Model',
-                to: 'https://chiapower.org',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/LebanonJon',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/jmhands/chiapower.github.io/',
-              },
-            ],
-          },
+        stylesheets: [
+            {
+                href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+                type: 'text/css',
+                integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+                crossorigin: 'anonymous',
+            },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Chiapower`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        themeConfig: {
+            navbar: {
+                title: 'Chiapower',
+                logo: {
+                    alt: 'Chiapower Logo',
+                    src: 'img/chiapower-logo.svg',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'intro',
+                        position: 'left',
+                        label: 'Model',
+                    },
+                    { to: '/blog', label: 'Blog', position: 'left' },
+                    {
+                        href: 'https://github.com/Chia-Network',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Model',
+                                to: 'https://chiapower.org',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Twitter',
+                                href: 'https://twitter.com/LebanonJon',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'Blog',
+                                to: '/blog',
+                            },
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/jmhands/chiapower.github.io/',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Chiapower`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        },
+    };
 };
-
-module.exports = config;
